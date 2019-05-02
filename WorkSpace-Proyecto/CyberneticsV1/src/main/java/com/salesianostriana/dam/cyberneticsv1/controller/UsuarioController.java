c3f5d67af5bd77300dcbf8ff2bdb70adf3fcadc3
Pepe <lealriveropepe@outlook.es>
2019-05-02 22:13:53 +0200

    .

--- WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/controller/UsuarioController.java
+++ WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/controller/UsuarioController.java
@@ -1,7 +1,19 @@
 package com.salesianostriana.dam.cyberneticsv1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/registro")
+@Controller
 public class UsuarioController {
 	
+	@GetMapping
+	public String registro(Model model) {
+		
+		return "registro";
+	}
 	
 
 }
