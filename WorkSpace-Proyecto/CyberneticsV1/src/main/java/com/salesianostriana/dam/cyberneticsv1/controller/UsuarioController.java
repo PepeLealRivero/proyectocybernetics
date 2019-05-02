335f01fa6a850f1bdec20b1779e83fb932c99e57
Pepe <lealriveropepe@outlook.es>
2019-05-02 22:09:49 +0200

    .

--- WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/controller/UsuarioController.java
+++ WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/controller/UsuarioController.java
@@ -1,8 +1,5 @@
 package com.salesianostriana.dam.cyberneticsv1.controller;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
 public class UsuarioController {
 	
 	
