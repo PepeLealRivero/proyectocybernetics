bc21ba35ddb98d96469e33dff4726c237f7848c1
Jose Leal <pepeldeloslakers@gmail.com>
2019-05-02 13:41:04 +0200

    .

--- WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/baseservices/BaseServices.java
+++ WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/baseservices/BaseServices.java
@@ -3,10 +3,46 @@
  */
 package com.salesianostriana.dam.cyberneticsv1.baseservices;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author jleal
  *
  */
-public class BaseServices {
+public abstract class BaseServices <T, ID, R extends JpaRepository<T, ID>>{
+	
+	@Autowired
+	R repositorio;
+
+	
+	
+	public T add(T T) {
+		return repositorio.save(T);
+	}
+
+	public T edit(T T) {
+		return repositorio.save(T);
+	}
+
+	public void delete(T t) {
+		repositorio.delete(t);
+	}
+
+	public void deleteById(ID id) {
+		repositorio.deleteById(id);
+	}
+
+	
+	public List<T> findAll(){
+		return repositorio.findAll();
+	}
+	
+	public T findById(ID id) {
+		return repositorio.findById(id).orElse(null);
+	}
+
 
 }
--- WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/service/PedidoService.java
+++ WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/service/PedidoService.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.salesianostriana.dam.cyberneticsv1.baseservices.BaseServices;
+import com.salesianostriana.dam.cyberneticsv1.model.Curso;
 import com.salesianostriana.dam.cyberneticsv1.model.Pedido;
+import com.salesianostriana.dam.cyberneticsv1.repository.CursoRepository;
 import com.salesianostriana.dam.cyberneticsv1.repository.PedidoRepository;
 
 
@@ -19,40 +22,9 @@ import com.salesianostriana.dam.cyberneticsv1.repository.PedidoRepository;
  */
 
 @Service
-public class PedidoService {
+public class PedidoService extends BaseServices<Pedido, Long, PedidoRepository>{
 
-	private PedidoRepository pedidoRepository;
 
-	/**
-	 * @param pedidoRepository
-	 */
-	public PedidoService(PedidoRepository pedidoRepository) {
-		this.pedidoRepository = pedidoRepository;
-	}
-
-	public Pedido add(Pedido ped) {
-		return pedidoRepository.save(ped);
-	}
-
-	public Pedido edit(Pedido ped) {
-		return pedidoRepository.save(ped);
-	}
-
-	public void delete(Pedido ped) {
-		pedidoRepository.delete(ped);
-	}
-
-	public void delete(long id) {
-		pedidoRepository.deleteById(id);
-	}
 
 	
-	public List<Pedido> findAll(){
-		return pedidoRepository.findAll();
-	}
-	
-	public Pedido findById(long id) {
-		return pedidoRepository.findById(id).orElse(null);
-	}
-
 }
