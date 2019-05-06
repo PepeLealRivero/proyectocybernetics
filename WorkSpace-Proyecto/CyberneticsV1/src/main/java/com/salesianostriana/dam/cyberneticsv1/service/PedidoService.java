81ef9c7d7ce52bb1ce3a95a6004bd7d81b78eb24
Jose Leal <pepeldeloslakers@gmail.com>
2019-05-06 13:31:32 +0200

    .

--- WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/service/PedidoService.java
+++ WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/service/PedidoService.java
@@ -3,14 +3,10 @@
  */
 package com.salesianostriana.dam.cyberneticsv1.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.cyberneticsv1.baseservices.BaseServices;
-import com.salesianostriana.dam.cyberneticsv1.model.Curso;
 import com.salesianostriana.dam.cyberneticsv1.model.Pedido;
-import com.salesianostriana.dam.cyberneticsv1.repository.CursoRepository;
 import com.salesianostriana.dam.cyberneticsv1.repository.PedidoRepository;
 
 
