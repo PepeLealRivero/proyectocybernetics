f0add7da0a4e7909fb938ce479c21c1f873f9345
Jose Leal <pepeldeloslakers@gmail.com>
2019-05-13 09:01:51 +0200

    .

--- WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/service/UsuarioService.java
+++ WorkSpace-Proyecto/CyberneticsV1/src/main/java/com/salesianostriana/dam/cyberneticsv1/service/UsuarioService.java
@@ -3,26 +3,59 @@
  */
 package com.salesianostriana.dam.cyberneticsv1.service;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.salesianostriana.dam.cyberneticsv1.baseservices.BaseServices;
 import com.salesianostriana.dam.cyberneticsv1.model.Usuario;
-import com.salesianostriana.dam.cyberneticsv1.repository.UsuarioRepository;
 
 /**
  * @author Jose
  *
  */
 @Service
-public class UsuarioService extends BaseServices<Usuario, Long, UsuarioRepository>{
+public class UsuarioService {
 
 	
 
-	public Usuario findOneByNomUser(String nomUser) {
-		return repositorio.findOneByNomUser(nomUser);
+	@Autowired
+	EntityManager entityManager;
+
+	public Usuario findUsuarioByUsername(String username) {
+
+		Usuario result = null;
+
+		TypedQuery<Usuario> query = 
+			entityManager.createQuery("select u from Usuario u where u.username = :username",
+				Usuario.class);
+
+		try {
+			result = query.setParameter("username", username).getSingleResult();
+		} catch (NoResultException | NonUniqueResultException ex) {
+			result = null;
+		}
+
+		return result;
 	}
-	
-	public Usuario findByEmail(String email) {
-		return repositorio.findByEmail(email);
+
+	public List<Usuario> findThemAll() {
+
+		List<Usuario> result = null;
+
+		TypedQuery<Usuario> query = entityManager.createQuery("select u from Usuario u", Usuario.class);
+
+		try {
+			result = query.getResultList();
+		} catch (NoResultException e) {
+			result = null;
+		}
+
+		return result;
 	}
 }
